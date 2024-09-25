30ede18651fdd49fe630e9594895583e703eea11
PatrickGQ <patrick.gonzales@ucb.edu.bo>
2024-09-25 19:15:09 -0400

    Se revirtieron los cambios ya que si se elimina esos parametros de entrada deja de funcionar el juego

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
-define(["model/sounds", "model/game", "controller/gameRunner", "controller/localStorageManager"],
-    function (modules) {
-        const { Sounds, Game, GameRunner, LSM } = modules;
-    
+define(["model/sounds", "model/images", "model/canvas", "model/character", "model/game", "view/draw", "controller/listener", "controller/action", "controller/keybind", "controller/gameRunner", "controller/localStorageManager"],
+    function (Sounds, Images, Canvas, Character, Game, Draw, Listener, Action, Keybind, GameRunner, LSM) {
         LSM.init();
         LSM.load();
-    
         if (!Game.muteMusic && window.requestAnimationFrame !== undefined) {
             Game.musicCreated = true;
             Sounds.bgMusic.play();
         }
-    
         Game.getScreen();
         Game.addStars();
         GameRunner.gameLoop();
