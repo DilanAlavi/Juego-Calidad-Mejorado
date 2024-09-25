e2ac7c8a33d85d11ac50a2887259b70da91bd2ad
PatrickGQ <patrick.gonzales@ucb.edu.bo>
2024-09-25 18:36:11 -0400

    Se soluciono la funcion con muchos parametros de entrada sin uso en el main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-define(["model/sounds", "model/images", "model/canvas", "model/character", "model/game", "view/draw", "controller/listener", "controller/action", "controller/keybind", "controller/gameRunner", "controller/localStorageManager"],
-    function (Sounds, Images, Canvas, Character, Game, Draw, Listener, Action, Keybind, GameRunner, LSM) {
+define(["model/sounds", "model/game", "controller/gameRunner", "controller/localStorageManager"],
+    function (modules) {
+        const { Sounds, Game, GameRunner, LSM } = modules;
+    
         LSM.init();
         LSM.load();
+    
         if (!Game.muteMusic && window.requestAnimationFrame !== undefined) {
             Game.musicCreated = true;
             Sounds.bgMusic.play();
         }
+    
         Game.getScreen();
         Game.addStars();
         GameRunner.gameLoop();
